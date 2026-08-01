8af7e7d4d5a7f9e5bd6124aeb9c396212dbf7fbd
Vikash Tiwari <vikashtiwari.fullstackdev@gmail.com>
2026-08-01 19:03:45 +0530

    Connect Flask with MySQL and implement user data insertion

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from database.db_connection import get_db_connection
 
 app = Flask(__name__)
 
@@ -6,6 +7,37 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+
+    full_name = request.form["full_name"]
+    email = request.form["email"]
+    phone = request.form["phone"]
+
+    connection = get_db_connection()
+
+    if connection:
+
+        cursor = connection.cursor()
+
+        query = """
+        INSERT INTO users (full_name, email, phone)
+        VALUES (%s, %s, %s)
+        """
+
+        values = (full_name, email, phone)
+
+        cursor.execute(query, values)
+
+        connection.commit()
+
+        cursor.close()
+
+        connection.close()
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
